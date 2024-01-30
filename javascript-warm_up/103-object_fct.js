2473cab17e08c3778fdbb78736e9dd238bd6b78d
fageer <fageern@gmail.com>
2024-01-30 21:51:29 +0300

    8. Increment object

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,12 +1,13 @@
 const myObject = {
   type: "object",
   value: 12,
-  incr: function () {
-    this.value++;
-  },
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
 };
 
-console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
