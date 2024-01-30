af9c97b17607d2445f1447ba31f728c551373765
fageer <fageern@gmail.com>
2024-01-30 21:06:31 +0300

    5. Square #0

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,10 @@
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+    constructor(w, h,size) {
+        super(w, h);
+        this.size = size;
+    };
+};
+
+module.exports = Square;
