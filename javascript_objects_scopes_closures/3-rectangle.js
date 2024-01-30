7160d2995f782f6a62d45a9c4b9f65f2b966188a
fageer <fageern@gmail.com>
2024-01-30 17:49:22 +0300

    3. Rectangle #3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -6,6 +6,7 @@ class Rectangle {
             this.height = h;
         }
     }
+
     print() {
         for (let i = 0; i < this.height; i++) {
             let line = '';
@@ -16,4 +17,5 @@ class Rectangle {
         };
     };  
 };
+
 module.exports = Rectangle;
