538f7e74dbfaa1662796ad80357c4ab683c02d9a
fageer <fageern@gmail.com>
2024-02-06 23:21:11 +0300

    2. Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -3,9 +3,11 @@ request(process.argv[2], function (error, response, body) {
     const responseBody = JSON.parse(body).results;
     let count = 0;
     responseBody.forEach(element => {
-        if (element.characters.includes("https://swapi-api.alx-tools.com/api/people/18/")) {
-            count++;
-        }
+        element.characters.forEach(character => {
+            if (character === "https://swapi-api.alx-tools.com/api/people/18/") {
+                count++;
+            }
+        });
     });
     console.log(count); 
 });
